3d2d6643918f54f49f36e3e1953e61f2e867b857
Askar Rizvi <askarrizvi88@gmail.com>
2021-10-27 16:05:56 -0400

    Fix success page for checkout

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -5,29 +5,9 @@ import { ADD_ORDER } from "../utils/mutations";
 import { idbPromise } from "../utils/helpers";
 
 function Success() {
-    const [addOrder] = useMutation(ADD_ORDER);
-
-    useEffect(() => {
-        async function saveOrder() {
-            const cart = await idbPromise('cart', 'get');
-            const products = cart.map(item => item._id);
-
-            if (products.length) {
-                const { data } = await addOrder({ variables: { products } });
-                const productData = data.addOrder.products;
-
-                productData.forEach((item) => {
-                    idbPromise('cart', 'delete', item);
-                });
-            }
-
-            setTimeout(() => {
-                window.location.assign('/');
-            }, 3000);
-        }
-
-        saveOrder();
-    }, [addOrder]);
+    setTimeout(() => {
+        window.location.assign('/');
+    }, 3000);
 
     return (
         <div>
@@ -38,6 +18,7 @@ function Success() {
                 </h2>
                 <h2>
                     You will now be redirected to the home page
+                    {setTimeout}
                 </h2>
             </Jumbotron>
         </div>
